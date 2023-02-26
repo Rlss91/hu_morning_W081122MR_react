3268ac636eb118b347a344cf92020ee08a9c09cd
Rlss91 <rlss91@gmail.com>
2023-02-26 15:57:17 +0200

    first component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import FirstComponent from "./components/FirstComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,10 @@
+const FirstComponent = () => {
+  return (
+    <div>
+      <h1>Hello component world</h1>
+      <h2>Hello component world</h2>
+    </div>
+  );
+};
+
+export default FirstComponent;
