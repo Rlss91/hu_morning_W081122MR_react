fd5b6a22da1ec589d0d60129269e6b7b395bc7d6
Rlss91 <rlss91@gmail.com>
2023-03-05 12:29:17 +0200

    events

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ListComponent from "./components/ListComponent";
 import MuiNavbar from "./components/Navbar/MuiNavbar";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       {/* <FirstComponent /> */}
       {/* <ListComponent /> */}
       {/* <GridComponent /> */}
-      <HomePage />
+      {/* <HomePage /> */}
+      <RegisterPage />
     </Container>
   );
 }
