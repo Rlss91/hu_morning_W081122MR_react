e8a1e45102d9c319eb6a615bd40088e62858525e
Rlss91 <rlss91@gmail.com>
2023-03-16 15:03:04 +0200

    joi msg

--- src/validation/loginValidation.js
+++ src/validation/loginValidation.js
@@ -5,9 +5,15 @@ import validation from "./validation";
 const loginSchema = Joi.object({
   email: Joi.string()
     .email({ tlds: { allow: false } })
+
     .required(),
   password: Joi.string()
     .pattern(new RegExp("^(?=.*[A-Z])(?=.*[a-z]).{0,}$"))
+    .messages({
+      "string.empty": "the password should not be empty",
+      "string.pattern.base":
+        "the password should be supper protected, this mean that its should contain only upper and lower case latter's",
+    })
     .min(2)
     .max(10)
     .required(),
